c150cfdc33ffd8053e574b6923fb634113c27d0f
Pedro Manoel <ps6711534@gmail.com>
2025-08-08 16:32:13 -0300

    parte do inventario feita

--- SistemaEstoque/urls.py
+++ SistemaEstoque/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('inventario/', views.inventario, name='inventario'),
     path('novo_item/', views.add_item, name='novo_item'),
     path('nova_categoria/', views.add_ctg, name='nova_categoria'),
+    path('inventario/', views.inventario , name='inventario'),
+    path('inventario/search/', views.search , name='search'),
 ]
--- SistemaEstoque/views/inventario.py
+++ SistemaEstoque/views/inventario.py
@@ -1,4 +1,43 @@
 from django.shortcuts import render
+from SistemaEstoque.models import Estoque,Categoria
+from django.core.paginator import Paginator
+from django.db.models import Q
+
 
 def inventario(request):
-    return render(request,'SistemaEstoque/inventario.html')
+    estoque=Estoque.objects.filter().order_by('-id',)
+    paginator=Paginator(estoque,30)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
+    
+    contexto={
+        'estoque':page_obj
+    }
+
+
+
+    return render(request,'SistemaEstoque/inventario.html',contexto)
+
+def search(request):
+    search_value=request.GET.get('q', '').strip() 
+    
+    estoque=Estoque.objects.filter(
+        Q(nome__icontains=search_value) | 
+        Q(quantidade__icontains=search_value) | 
+        Q(id__icontains=search_value) |
+        Q(preco__icontains=search_value) 
+        )\
+    .order_by('-id',)
+    
+    paginator=Paginator(estoque, 30)
+    
+    page_number= request.GET.get('page')
+    
+    page_obj= paginator.get_page(page_number)
+
+    contexto={
+        'search_value' :search_value,
+        'estoque': page_obj,
+    }
+
+    return render(request, 'SistemaEstoque/inventario.html', contexto)
